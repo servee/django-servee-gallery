1f0a5c9bbacd73dab516a49d0f34648676fa4774
Issac Kelly <issac.kelly@gmail.com>
2011-03-20 23:31:28 -0400

    Add license and fix setup.

--- servee_image/__init__.py
+++ servee_image/__init__.py
@@ -1,51 +1,16 @@
-VERSION = (0, 1, 'alpha', 1)
+VERSION = (0, 1, 0, "a", 1)  # following PEP 386
+DEV_N = None
 
 
-def get_version(join=' ', short=False):
-    """
-    Return the version of this package as a string.
+def get_version():
+    version = "%s.%s" % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = "%s.%s" % (version, VERSION[2])
+    if VERSION[3] != "f":
+        version = "%s%s%s" % (version, VERSION[3], VERSION[4])
+        if DEV_N:
+            version = "%s.dev%s" % (version, DEV_N)
+    return version
 
-    The version number is built from a ``VERSION`` tuple, which should consist
-    of integers, or trailing version information (such as 'alpha', 'beta' or
-    'final'). For example:
 
-    >>> VERSION = (2, 0, 6)
-    >>> get_version()
-    '2.0.6'
-
-    >>> VERSION = (1, 0, 'beta', 2)
-    >>> get_version()
-    '1.0 beta 2'
-
-    Use the ``join`` argument to join the version elements by an alternate
-    character to the default ``' '``. This is useful when building a distutils
-    setup module::
-
-        from this_package import get_version
-
-        setup(
-            version=get_version(join='-'),
-            # ...
-        )
-
-    Use the ``short`` argument to get the version number without trailing
-    version information.
-
-    """
-    version = []
-    number = []
-    remainder = []
-    for i, bit in enumerate(VERSION):
-        if isinstance(bit, int):
-            number.append(str(bit))
-        else:
-            remainder = [str(bit) for bit in VERSION[i:]]
-            break
-    if number:
-        version.append('.'.join(number))
-    if not short:
-        if remainder == ['alpha', 0]:
-            version.append('pre-alpha')
-        elif 'final' not in remainder:
-            version.extend(remainder)
-    return join.join(version)
+__version__ = get_version()
