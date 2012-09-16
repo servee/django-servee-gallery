5f8940ebf14b86bc927175c264e0367e04ccd417
Issac Kelly <issac.kelly@gmail.com>
2012-09-16 12:25:32 -0700

    Whitespace cleanup, model title, minor version release.

--- servee_gallery/models.py
+++ servee_gallery/models.py
@@ -5,26 +5,32 @@ import datetime
 
 class BaseGallery(models.Model):
     """
-    BaseGallery 
+    BaseGallery
     """
     title = models.CharField(max_length=256, blank=True, null=True)
     slug = models.SlugField(max_length=128, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     created = models.DateTimeField(blank=True)
     modified = models.DateTimeField(blank=True)
-    
+
     def save(self, *args, **kwargs):
         self.modified = datetime.datetime.now()
-        
+
         if not self.slug:
             self.slug = slugify(self.title)
-        
+
         if not self.created:
             self.created = datetime.datetime.now()
         super(BaseGallery, self).save(*args, **kwargs) # Call the "real" save() method.
-    
+
     class Meta:
         abstract = True
+
+    def __unicode__(self):
+        if self.title:
+            return self.title
+        else:
+            return u"Gallery #%s" % self.id
 
 class Gallery(BaseGallery):
     """
@@ -46,25 +52,25 @@ class BaseGalleryItem(models.Model):
 
     def __unicode__(self):
         return "%s" % self.title
-        
+
     def save(self, *args, **kwargs):
         self.modified = datetime.datetime.now()
-        
+
         # update gallery modified time
         self.gallery.modified = datetime.datetime.now()
         self.gallery.save()
-        
+
         if not self.uploaded:
             self.uploaded = datetime.datetime.now()
         super(BaseGalleryItem, self).save(*args, **kwargs) # Call the "real" save() method.
-        
+
     def get_absolute_url(self):
         return self.item.url
 
     class Meta:
         abstract = True
         ordering = ("gallery", "order")
-    
+
 class Image(BaseGalleryItem):
     """
     """
@@ -72,14 +78,14 @@ class Image(BaseGalleryItem):
         upload_to=IMAGE_UPLOAD_TO,
         storage=DEFAULT_STORAGE()
     )
-    
+
     def save(self, *args, **kwargs):
         # create a title if the galleritem has a name
         if not self.title:
             self.title = self.image.name
         super(Image, self).save(*args, **kwargs) # Call the "real" save() method.
-    
-    class Meta:        
+
+    class Meta:
         verbose_name = "servee gallery image"
         verbose_name_plural = "servee gallery images"
         ordering = ["modified",]
